92f58b6ae9bb708770517c24a38eb23cc75ef84d
Daniel Rost <daniel.rost@gmx.de>
2014-10-25 15:58:53 +0200

    minor optimizations

--- src/de/com/rost/simplegame/Game.java
+++ src/de/com/rost/simplegame/Game.java
@@ -6,8 +6,8 @@ public class Game {
 
 		Player marc = new Player("Marc");
 		Player peter = new Player("Peter");
-		Magazine lifestyle = new Magazine("Lifestyle", 4.50);
-		Magazine ct = new Magazine("c't", 7.90);
+//		Magazine lifestyle = new Magazine("Lifestyle", 4.50);
+//		Magazine ct = new Magazine("c't", 7.90);
 		Room dungeon = new Room("Dungeon");
 		dungeon.placePlayer(marc, peter);
 		dungeon.listPlayers();
